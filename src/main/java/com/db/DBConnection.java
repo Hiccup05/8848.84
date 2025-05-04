e7590edf5aa810ba220fd136621bbba066e01517
chiragtmg <chiragtmg456@gmail.com>
2025-05-04 10:29:07 +0545

    added git ignore

--- src/main/java/com/db/DBConnection.java
+++ src/main/java/com/db/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 		Connection conn=null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn=DriverManager.getConnection("jdbc:mysql://localhost:5222/cafe","root","");
+			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/cafe","root","");
 		}
 		catch(Exception e) {
 			e.printStackTrace();
